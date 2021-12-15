7ce9d9e633efdc26f2403d9d6c1aed11792ac58b
jackkrone <jack.krone@boweryvaluation.com>
2021-12-15 15:06:15 -0600

    Inject TasksService into TasksController

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // following line is TS syntatic-sugar
+  // TS auto creates a private property called tasksService with a value of TasksService
+  constructor(private tasksService: TasksService) {}
+}
