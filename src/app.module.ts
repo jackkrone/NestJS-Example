1747c01f24dbd80ff112a58286e6d33f290795de
jackkrone <jack.krone@boweryvaluation.com>
2021-12-15 14:47:08 -0600

    create tasks module and tasks controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
 
 // "This is a class that is decorated with a decorate [@Module] coming from the @nestjs/common module"
 // "This basically turns this class into a module"
 @Module({
-  imports: [],
+  imports: [TasksModule],
 })
 export class AppModule {}
