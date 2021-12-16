813dfad7a722ab53ff12930b58c62570ffa470ea
jackkrone <jack.krone@boweryvaluation.com>
2021-12-16 12:46:28 -0600

    Add feature to get task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './DTOs/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
@@ -9,11 +9,18 @@ export class TasksController {
   // TS auto creates a private property called tasksService with a value of TasksService
   constructor(private tasksService: TasksService) {}
 
-  // Whenever a get request comes in to the TasksController, the getAllTasks method handler will handle it
-  // It's not necessary to add the return type as TS infers it, but I have it anyways (two lines below)
+  // When a get request comes in to the TasksController, it's handled by the appropriate Get handler below
+  // It's probably not necessary to add the return type as TS infers it, but I have it anyways (two lines below)
+  // localhost:3000/tasks
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // localhost:3000/tasks/lk234hg438j
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   // When a post request is received, Nest will assign values from the request body
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,6 +14,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  public getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   public createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
 
