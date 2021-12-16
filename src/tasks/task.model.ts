7bbb0fffc750c494079edb622751b35aac2c80b9
jackkrone <jack.krone@boweryvaluation.com>
2021-12-16 15:49:41 -0600

    Fix typos and lint

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,6 +1,6 @@
 import { TaskStatus } from 'src/enums';
 
-// "We can a model as a class or an interface"
+// "We can a define model as a class or an interface"
 // interface will do for now, it is more simple than using a class
 export interface Task {
   id: string;
