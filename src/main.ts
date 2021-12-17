090565aab1e48329047602da17df861a70bfd4db
jackkrone <jack.krone@boweryvaluation.com>
2021-12-17 10:39:05 -0600

    Implement task creation validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // All this function does is creat and start e a new nest server. We pass it our root module AppModule
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Telling NestJS: "whenever you encounter a validation decorator I want you to run your validation pipe"
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/tasks/DTOs/create-task.dto.ts
+++ src/tasks/DTOs/create-task.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
+
 // Class name below is proper NestJS naming convention for DTOs
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
