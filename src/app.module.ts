18a794144bc82e61698dc64984caa4d23184e903
jackkrone <jack.krone@boweryvaluation.com>
2021-12-17 15:44:01 -0600

    Connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 // "This is a class that is decorated with a decorate [@Module] coming from the @nestjs/common module"
 // "This basically turns this class into a module"
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true, // keeps DB synched... whatever that means
+    }),
+  ],
 })
 export class AppModule {}
