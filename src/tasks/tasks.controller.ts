71d61fc712de54f8ae4a10a054da3b631378d46f
jackkrone <jack.krone@boweryvaluation.com>
2021-12-20 11:38:38 -0600

    convert getTaskById to TypeORM implementation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,6 +11,7 @@ import {
 import { CreateTaskDto } from './DTOs/create-task.dto';
 import { GetTasksFilterDto } from './DTOs/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './DTOs/update-task-status.dto';
+import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -19,50 +20,50 @@ export class TasksController {
   // TS auto creates a private property called tasksService with a value of TasksService
   constructor(private tasksService: TasksService) {}
 
-  /* // When a get request comes in to the TasksController, it's handled by the appropriate Get handler below
-  // localhost:3000/tasks
-  @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    // Run tasksService.getTasksWithFilters if filters are define
-    // Run tasksService.getAllTasks if no filters defined
-    if (Object.keys(filterDto).length) {
-      return this.tasksService.getTasksWithFilters(filterDto);
-    } else {
-      return this.tasksService.getAllTasks();
-    }
-  }
+  // // When a get request comes in to the TasksController, it's handled by the appropriate Get handler below
+  // // localhost:3000/tasks
+  // @Get()
+  // getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  //   // Run tasksService.getTasksWithFilters if filters are define
+  //   // Run tasksService.getAllTasks if no filters defined
+  //   if (Object.keys(filterDto).length) {
+  //     return this.tasksService.getTasksWithFilters(filterDto);
+  //   } else {
+  //     return this.tasksService.getAllTasks();
+  //   }
+  // }
 
   // localhost:3000/tasks/lk234hg438j
   @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
+  getTaskById(@Param('id') id: string): Promise<Task> {
     return this.tasksService.getTaskById(id);
   }
 
-  // When a post request is received, Nest will assign values from the request body
-  // ... to the appropriate parameters as defined by the CreateTaskDto class
-  @Post()
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto);
-  }
+  // // When a post request is received, Nest will assign values from the request body
+  // // ... to the appropriate parameters as defined by the CreateTaskDto class
+  // @Post()
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //   return this.tasksService.createTask(createTaskDto);
+  // }
 
-  // localhost:3000/tasks/lk234hg438j
-  @Delete('/:id')
-  deleteTaskById(@Param('id') id: string): void {
-    // not necessary to includ return statemetn but also not problematic here
-    return this.tasksService.deleteTaskById(id);
-  }
+  // // localhost:3000/tasks/lk234hg438j
+  // @Delete('/:id')
+  // deleteTaskById(@Param('id') id: string): void {
+  //   // not necessary to includ return statemetn but also not problematic here
+  //   return this.tasksService.deleteTaskById(id);
+  // }
 
-  // HTTP Patch request is to update
-  // The new status is sent in the body
-  // status in the URL (non-variable) is just to indicate what the patch is doing
-  // Not worthwhile to use DTOs because you need a different one for both Param and Body
-  // Each only have one value associated with them so DTO wouldn't save time
-  @Patch('/:id/status')
-  updateTaskStatusById(
-    @Param('id') id: string,
-    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
-  ): Task {
-    const { status } = updateTaskStatusDto;
-    return this.tasksService.updateTaskStatusById(id, status);
-  } */
+  // // HTTP Patch request is to update
+  // // The new status is sent in the body
+  // // status in the URL (non-variable) is just to indicate what the patch is doing
+  // // Not worthwhile to use DTOs because you need a different one for both Param and Body
+  // // Each only have one value associated with them so DTO wouldn't save time
+  // @Patch('/:id/status')
+  // updateTaskStatusById(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  // ): Task {
+  //   const { status } = updateTaskStatusDto;
+  //   return this.tasksService.updateTaskStatusById(id, status);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,90 +3,89 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { TaskStatus } from 'src/enums';
 import { CreateTaskDto } from './DTOs/create-task.dto';
 import { GetTasksFilterDto } from './DTOs/get-tasks-filter.dto';
+import { Task } from './task.entity'; // Now we import a task entity instead of a task model
 import { TasksRepository } from './tasks.repository';
 
 @Injectable()
 export class TasksService {
   constructor(
-    @InjectRepository(TasksService)
+    @InjectRepository(TasksRepository)
     private tasksRepository: TasksRepository,
   ) {}
 
-  /*   // It's not necessary to use the public keyword
-  // It's also not necessary to add the return type as TS infers it
-  public getAllTasks(): Task[] {
-    return this.tasks;
-  }
+  // // It's not necessary to use the public keyword
+  // // It's also not necessary to add the return type as TS infers it
+  // public getAllTasks(): Task[] {
+  //   return this.tasks;
+  // }
 
-  public getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
+  // public getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+  //   const { status, search } = filterDto;
 
-    // define temp arr to hold result
-    // Is this a proper copy of the array? Given that JS passes objects by reference
-    let tasks = this.getAllTasks();
+  //   // define temp arr to hold result
+  //   // Is this a proper copy of the array? Given that JS passes objects by reference
+  //   let tasks = this.getAllTasks();
 
-    // filter with status
-    if (status) {
-      tasks = tasks.filter((task) => task.status === status);
-    }
+  //   // filter with status
+  //   if (status) {
+  //     tasks = tasks.filter((task) => task.status === status);
+  //   }
 
-    // filter with search
-    // would help to use .toLowerCase()
-    if (search) {
-      tasks = tasks.filter(
-        (task) =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
+  //   // filter with search
+  //   // would help to use .toLowerCase()
+  //   if (search) {
+  //     tasks = tasks.filter(
+  //       (task) =>
+  //         task.title.includes(search) || task.description.includes(search),
+  //     );
+  //   }
 
-    // return final result
-    return tasks;
-  }
+  //   // return final result
+  //   return tasks;
+  // }
 
-  public getTaskById(id: string): Task {
-    // try to get task
-    const found = this.tasks.find((task) => task.id === id);
-    // return task or if not found, throw error (404 not found)
+  // Must define as async because we are interacting with a databse now
+  // return type must now be a promise
+  public async getTaskById(id: string): Promise<Task> {
+    const found = await this.tasksRepository.findOne(id); // findOne is a typeORM method, returns first entry that satisfies matching condition
+
+    // Throw exception if nothing is found
     if (!found) {
-      // NestJS will auto handle this error and map it to appropriate HTTP response code
-      // If it throws
-      // Wrap in try-catch block to handle it yourself
       throw new NotFoundException(`Task with ID ${id} not found`);
-      // In general, if an error is thrown and Nest can't map it to a code, it defaults to
-      // ... 500 Internal server error
     }
+
     return found;
   }
 
-  public createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
+  // public createTask(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
 
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      // The following construction is ideal because you can change the value of OPEN
-      // ... in the enums file without having to change anything here
-      status: TaskStatus.OPEN,
-    };
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     // The following construction is ideal because you can change the value of OPEN
+  //     // ... in the enums file without having to change anything here
+  //     status: TaskStatus.OPEN,
+  //   };
 
-    this.tasks.push(task);
-    return task;
-  }
+  //   this.tasks.push(task);
+  //   return task;
+  // }
 
-  public deleteTaskById(id: string): void {
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter((task) => task.id !== found.id);
-    // no need to return anything
-  }
+  // public deleteTaskById(id: string): void {
+  //   const found = this.getTaskById(id);
+  //   this.tasks = this.tasks.filter((task) => task.id !== found.id);
+  //   // no need to return anything
+  // }
 
-  public updateTaskStatusById(id: string, status: TaskStatus): Task {
-    // update task of given id
-    // very simple because JavaScript passes objects by reference
-    // it's bad practice to mutate the task directly
-    // would be more bulletproof code if you replace it after validating that it exists
-    const task = this.getTaskById(id);
-    task.status = status;
-    return task;
-  } */
+  // public updateTaskStatusById(id: string, status: TaskStatus): Task {
+  //   // update task of given id
+  //   // very simple because JavaScript passes objects by reference
+  //   // it's bad practice to mutate the task directly
+  //   // would be more bulletproof code if you replace it after validating that it exists
+  //   const task = this.getTaskById(id);
+  //   task.status = status;
+  //   return task;
+  // }
 }
