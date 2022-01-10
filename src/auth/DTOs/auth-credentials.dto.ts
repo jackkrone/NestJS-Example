d3141cda1af6651d22d7acbffac63e68714c7f2f
jackkrone <jack.krone@boweryvaluation.com>
2022-01-10 15:59:59 -0600

    Enable user and pass signup validation

--- src/auth/DTOs/auth-credentials.dto.ts
+++ src/auth/DTOs/auth-credentials.dto.ts
@@ -1,5 +1,15 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 // recall: the Dto is essentially just a typescript type, can be done with interface or class
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/) // This requires password to match a regular expression
   password: string;
 }
